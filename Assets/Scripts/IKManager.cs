0fc4f204b03248e6b45017ea29acd8caf2179a2d
HansNebula <hatfansahrul1@gmail.com>
2024-07-10 05:54:31 +0700

    add new paran to calculate slope

--- Assets/Scripts/IKManager.cs
+++ Assets/Scripts/IKManager.cs
@@ -22,30 +22,38 @@ public class IKManager : MonoBehaviour
     public float m_rate = 5f;
     public int m_steps=20;
 
-    float calculateSlope(Joint joint_){
+    public float posDef_x = 3f, posDef_y = 3f, posDef_z=0;
+
+    public robotState generalState;
+
+    float calculateSlope(Joint joint_, Vector3 heading){
         float deltaTheta = 0.01f;
-        float distance1 = getDistance(m_end.transform.position, m_target.transform.position);
+        float distance1 = getDistance(m_end.transform.position, heading);
 
         joint_.Rotate(deltaTheta);
 
-        float distance2 = getDistance(m_end.transform.position, m_target.transform.position);
+        float distance2 = getDistance(m_end.transform.position, heading);
         joint_.Rotate(-deltaTheta);
         return (distance2 - distance1) / deltaTheta;
     }
 
-    float calcBaseSlope(TurnableBase joint_){
+    float calcBaseSlope(TurnableBase joint_, Vector3 heading){
         float deltaTheta = 0.01f;
-        float distance1 = getDistance(m_end.transform.position, m_target.transform.position);
+        float distance1 = getDistance(m_end.transform.position, heading);
 
         joint_.Rotate(deltaTheta);
 
-        float distance2 = getDistance(m_end.transform.position, m_target.transform.position);
+        float distance2 = getDistance(m_end.transform.position, heading);
         joint_.Rotate(-deltaTheta);
         return (distance2 - distance1) / deltaTheta;
     }
 
+    void Start(){
+        generalState = robotState.pose_def_;
+    }
     void Update(){
         // spontan();
+        prosedur();
     }
 
     float getDistance(Vector3 point1_, Vector3 point2_){
@@ -57,11 +65,11 @@ public class IKManager : MonoBehaviour
             if(getDistance(m_end.transform.position, m_target.transform.position) > m_threshold){
                 Joint current = m_root;
                 
-                float baseSlope = calcBaseSlope(m_base);
+                float baseSlope = calcBaseSlope(m_base, m_target.transform.position);
                 m_base.Rotate(-baseSlope * m_rate);
 
                 while(current != null){
-                    float slope = calculateSlope(current);
+                    float slope = calculateSlope(current, m_target.transform.position);
                     current.Rotate(-slope*m_rate);
                     current = current.GetChild();
                 }
@@ -69,7 +77,6 @@ public class IKManager : MonoBehaviour
         }
     }
 
-    public robotState generalState;
     public void prosedur(){
         switch(generalState){
             case robotState.pose_def_   : RobotPosDef(); break;
@@ -78,7 +85,31 @@ public class IKManager : MonoBehaviour
         }
     }
 
+    int ab=0;
     public void RobotPosDef(){
+        Vector3 calib=new Vector3(m_base.transform.position.x, m_base.transform.position.y + 100f, m_base.transform.position.z);
+        Vector3 heading=new Vector3(m_base.transform.position.x + posDef_x, m_base.transform.position.y + posDef_y, m_base.transform.position.z + posDef_z);
+
+        for(int i=0;i<m_steps;++i){
+                print("ab "+ab);
+            if(getDistance(m_end.transform.position, calib) > m_threshold && ab<1){
+                Joint current = m_root;
+                while(current != null){
+                    float slope = calculateSlope(current, calib);
+                    current.Rotate(-slope*10f);
+                    current = current.GetChild();
+                }
+                ab++;
+                print("ab "+ab);
+            }else if(getDistance(m_end.transform.position, heading) > m_threshold && ab!=0){
+                Joint current = m_root;
+                while(current != null){
+                    float slope = calculateSlope(current, heading);
+                    current.Rotate(-slope*m_rate);
+                    current = current.GetChild();
+                }
+            }
+        }
 
     }
 
