8d05fd3c9fda3823334399cac87092a249b9d995
HansNebula <hatfansahrul1@gmail.com>
2024-07-11 14:25:29 +0700

    delete useless code

--- Assets/Scripts/TurnableBase.cs
+++ Assets/Scripts/TurnableBase.cs
@@ -6,17 +6,7 @@ public class TurnableBase : MonoBehaviour
 {
     public float rotationSpeed = 0.5f;
     public void Rotate(float angle_){
-        // Vector3 direction = target - transform.position;
-
-        // // Calculate the angle around the Z axis to look at the target
-        // float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // // Create a rotation around the Z axis
-        // Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        // // Apply the rotation to the object
-        // transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
-
+        
         transform.Rotate(Vector3.forward * angle_);
     }
 }
